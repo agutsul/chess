394e85e4cd12b0a1758078021f8be01a2c892770
Andrii Gutsul <agutsulrepo@gmail.com>
2024-10-10 15:38:19 -0400

    minor refactoring

--- src/main/java/com/agutsul/chess/exception/IllegalBoardException.java
+++ src/main/java/com/agutsul/chess/exception/IllegalBoardException.java
@@ -1,6 +1,7 @@
 package com.agutsul.chess.exception;
 
-public class IllegalBoardException extends RuntimeException {
+public class IllegalBoardException
+        extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
