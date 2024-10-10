1bddbcaac86d96a8ca22b13770f6f939f3a4060b
Andrii Gutsul <agutsulrepo@gmail.com>
2024-10-10 12:01:24 -0400

    add more logs

--- src/main/java/com/agutsul/chess/player/state/ActivePlayerState.java
+++ src/main/java/com/agutsul/chess/player/state/ActivePlayerState.java
@@ -1,11 +1,17 @@
 package com.agutsul.chess.player.state;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
 import com.agutsul.chess.event.Observable;
 import com.agutsul.chess.player.Player;
 import com.agutsul.chess.player.event.RequestPlayerActionEvent;
 
 public class ActivePlayerState
         extends AbstractPlayerState {
+
+    private static final Logger LOGGER = getLogger(ActivePlayerState.class);
 
     private final Observable observable;
 
@@ -16,6 +22,7 @@ public class ActivePlayerState
 
     @Override
     public void play(Player player) {
+        LOGGER.info("Request player '{}' action", player);
         observable.notifyObservers(new RequestPlayerActionEvent(player));
     }
 }
--- src/main/java/com/agutsul/chess/player/state/LockedPlayerState.java
+++ src/main/java/com/agutsul/chess/player/state/LockedPlayerState.java
@@ -1,9 +1,15 @@
 package com.agutsul.chess.player.state;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
 import com.agutsul.chess.player.Player;
 
 public class LockedPlayerState
         extends AbstractPlayerState {
+
+    private static final Logger LOGGER = getLogger(LockedPlayerState.class);
 
     public LockedPlayerState() {
         super(Type.LOCKED);
@@ -11,6 +17,7 @@ public class LockedPlayerState
 
     @Override
     public void play(Player player) {
+        LOGGER.info("Request player '{}' action", player);
         // do nothing
     }
 }
