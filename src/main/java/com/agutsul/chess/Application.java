127ba3b2fe10eb741f55fcb7ccb7cba5105b25d3
Andrii Gutsul <agutsulrepo@gmail.com>
2024-11-01 21:27:53 -0400

    improve Application class

--- src/main/java/com/agutsul/chess/Application.java
+++ src/main/java/com/agutsul/chess/Application.java
@@ -4,9 +4,15 @@ import com.agutsul.chess.color.Colors;
 import com.agutsul.chess.console.ConsoleGame;
 import com.agutsul.chess.player.UserPlayer;
 
-public class Application {
+public class Application
+        implements Executable {
 
     public static void main(String[] args) {
+        new Application().execute();
+    }
+
+    @Override
+    public void execute() {
         var game = new ConsoleGame(
                 new UserPlayer("player1", Colors.WHITE),
                 new UserPlayer("player2", Colors.BLACK)
