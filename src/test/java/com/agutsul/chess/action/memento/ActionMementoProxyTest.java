415f5690e20b9a37f5c847a1d47a1c39ede51afa
Andrii Gutsul <agutsulrepo@gmail.com>
2024-11-04 22:46:52 -0500

    add test

---
+++ src/test/java/com/agutsul/chess/action/memento/ActionMementoProxyTest.java
@@ -0,0 +1,68 @@
+package com.agutsul.chess.action.memento;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ActionMementoProxyTest {
+
+    @Test
+    void testGetColor() {
+        var memento = mock(ActionMemento.class);
+
+        @SuppressWarnings("unchecked")
+        var proxy = new ActionMementoProxy<>(memento);
+        proxy.getColor();
+
+        verify(memento, times(1)).getColor();
+    }
+
+    @Test
+    void testGetActionType() {
+        var memento = mock(ActionMemento.class);
+
+        @SuppressWarnings("unchecked")
+        var proxy = new ActionMementoProxy<>(memento);
+        proxy.getActionType();
+
+        verify(memento, times(1)).getActionType();
+    }
+
+    @Test
+    void testGetPieceType() {
+        var memento = mock(ActionMemento.class);
+
+        @SuppressWarnings("unchecked")
+        var proxy = new ActionMementoProxy<>(memento);
+        proxy.getPieceType();
+
+        verify(memento, times(1)).getPieceType();
+    }
+
+    @Test
+    void testGetSource() {
+        var memento = mock(ActionMemento.class);
+
+        @SuppressWarnings("unchecked")
+        var proxy = new ActionMementoProxy<>(memento);
+        proxy.getSource();
+
+        verify(memento, times(1)).getSource();
+    }
+
+    @Test
+    void testGetTarget() {
+        var memento = mock(ActionMemento.class);
+
+        @SuppressWarnings("unchecked")
+        var proxy = new ActionMementoProxy<>(memento);
+        proxy.getTarget();
+
+        verify(memento, times(1)).getTarget();
+    }
+}
