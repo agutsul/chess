aa23600ba6030f007499e7ad771f2a157350c2c1
Andrii Gutsul <agutsulrepo@gmail.com>
2024-11-11 15:36:21 -0500

    adjust board state priorities

--- src/main/java/com/agutsul/chess/board/state/BoardState.java
+++ src/main/java/com/agutsul/chess/board/state/BoardState.java
@@ -15,13 +15,13 @@ public interface BoardState
     // https://en.wikipedia.org/wiki/Draw_(chess)
     enum Type {
         DEFAULT(9, false),
-        CHECKED(2, false),
+        CHECKED(5, false),
         CHECK_MATED(1, true),
-        STALE_MATED(3, true),            // draw - when the player to move is not in check but has no legal move ( automatic )
+        STALE_MATED(2, true),            // draw - when the player to move is not in check but has no legal move ( automatic )
         THREE_FOLD_REPETITION(7, false), // draw - when the same position occurs three times with the same player to move ( claims arbiter )
-        FIVE_FOLD_REPETITION(4, true),   // draw - when the same position occurs five times with the same player to move  ( automatic )
+        FIVE_FOLD_REPETITION(3, true),   // draw - when the same position occurs five times with the same player to move  ( automatic )
         FIFTY_MOVES(8, false),           // draw - when the last fifty successive moves made by both players contain no capture or pawn move ( claims arbiter )
-        SEVENTY_FIVE_MOVES(5, true),     // draw - when the last seventy five successive moves made by both players contain no capture or pawn move ( automatic ). If the last move was a checkmate, the checkmate stands.
+        SEVENTY_FIVE_MOVES(4, true),     // draw - when the last seventy five successive moves made by both players contain no capture or pawn move ( automatic ). If the last move was a checkmate, the checkmate stands.
         INSUFFICIENT_MATERIAL(6, false), // draw - ( dead position ) when no sequence of legal moves can lead to checkmate
         AGREED_DRAW(0, true);            // draw - when player agree to a draw ( player request )
 
