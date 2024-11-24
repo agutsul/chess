34699f78fe7946362f8b8d94c08e01ae9a04a2a1
Andrii Gutsul <agutsulrepo@gmail.com>
2024-11-23 23:26:17 -0500

    fix pmd warning

--- src/main/java/com/agutsul/chess/pgn/action/PawnCaptureActionAdapter.java
+++ src/main/java/com/agutsul/chess/pgn/action/PawnCaptureActionAdapter.java
@@ -49,9 +49,12 @@ final class PawnCaptureActionAdapter
     }
 
     @Override
-    Optional<Piece<Color>> getCapturablePiece(Piece.Type pieceType, String code, String position) {
+    Optional<Piece<Color>> getCapturablePiece(Piece.Type pieceType,
+                                              String code,
+                                              String position) {
+
         var pieces = board.getPieces(color, pieceType).stream()
-                .filter(piece -> code == null ? true : contains(codeOf(piece.getPosition()), code))
+                .filter(piece -> code == null || contains(codeOf(piece.getPosition()), code))
                 .toList();
 
         for (var piece : pieces) {
