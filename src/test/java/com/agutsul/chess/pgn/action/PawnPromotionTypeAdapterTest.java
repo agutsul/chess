405ddc3a8ba52f35b2fb33aadc062b7107fc810f
Andrii Gutsul <agutsulrepo@gmail.com>
2024-11-23 23:00:15 -0500

    cover PawnPromotionTypeAdapter with inut test

---
+++ src/test/java/com/agutsul/chess/pgn/action/PawnPromotionTypeAdapterTest.java
@@ -0,0 +1,61 @@
+package com.agutsul.chess.pgn.action;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.agutsul.chess.board.Board;
+import com.agutsul.chess.board.BoardBuilder;
+import com.agutsul.chess.color.Colors;
+import com.agutsul.chess.exception.IllegalActionException;
+
+@ExtendWith(MockitoExtension.class)
+public class PawnPromotionTypeAdapterTest {
+
+    @Mock
+    Board board;
+
+    @Test
+    void testAdaptInvalidPawnPromotionTypeAction() {
+        var adapter = new PawnPromotionTypeAdapter(board, Colors.WHITE);
+
+        for (var action : List.of("z9Q", "a0R", "2aQ", "A2r", "Aa22", "a11", "cxb3", "e4e4")) {
+            var thrown = assertThrows(
+                    IllegalActionException.class,
+                    () -> adapter.adapt(action)
+            );
+
+            var expectedMessage = String.format("Invalid action format: '%s'", action);
+            assertEquals(expectedMessage, thrown.getMessage());
+        }
+    }
+
+    @Test
+    void testAdaptPawnPromotionTypeMoveAction() {
+        var promotionBoard = new BoardBuilder().withWhitePawn("b7").build();
+
+        var adapter = new PawnPromotionTypeAdapter(promotionBoard, Colors.WHITE);
+        var action = adapter.adapt("b8Q");
+
+        assertEquals("Q", action);
+    }
+
+    @Test
+    void testAdaptPiecePromoteCaptureAction() {
+        var promotionBoard = new BoardBuilder()
+                .withWhitePawn("e7")
+                .withBlackKnight("f8")
+                .build();
+
+        var adapter = new PawnPromotionTypeAdapter(promotionBoard, Colors.WHITE);
+        var action = adapter.adapt("exf8R");
+
+        assertEquals("R", action);
+    }
+}
