dda7a577388a47ae9c4959f3fb5ac9c1fb4b8a26
Andrii Gutsul <agutsulrepo@gmail.com>
2024-12-05 20:30:37 -0500

    re-factor PawnPromoteAlgo with composite class

--- src/main/java/com/agutsul/chess/piece/pawn/PawnPromoteAlgo.java
+++ src/main/java/com/agutsul/chess/piece/pawn/PawnPromoteAlgo.java
@@ -1,14 +1,13 @@
 package com.agutsul.chess.piece.pawn;
 
 import java.util.Collection;
-import java.util.List;
 
 import com.agutsul.chess.board.Board;
 import com.agutsul.chess.color.Color;
 import com.agutsul.chess.piece.PawnPiece;
 import com.agutsul.chess.piece.algo.AbstractAlgo;
-import com.agutsul.chess.piece.algo.Algo;
 import com.agutsul.chess.piece.algo.CapturePieceAlgo;
+import com.agutsul.chess.piece.algo.CompositePieceAlgo;
 import com.agutsul.chess.piece.algo.MovePieceAlgo;
 import com.agutsul.chess.piece.algo.PromotePieceAlgo;
 import com.agutsul.chess.position.Position;
@@ -18,23 +17,23 @@ final class PawnPromoteAlgo<COLOR extends Color,
         extends AbstractAlgo<PAWN,Position>
         implements PromotePieceAlgo<COLOR,PAWN,Position> {
 
-    private final Collection<Algo<PAWN,Collection<Position>>> algos;
+    private final CompositePieceAlgo<COLOR,PAWN,Position> algo;
     private final int promotionLine;
 
+    @SuppressWarnings("unchecked")
     PawnPromoteAlgo(Board board, int promotionLine,
                     MovePieceAlgo<COLOR,PAWN,Position> moveAlgo,
                     CapturePieceAlgo<COLOR,PAWN,Position> captureAlgo) {
 
         super(board);
         this.promotionLine = promotionLine;
-        this.algos = List.of(moveAlgo, captureAlgo);
+        this.algo = new CompositePieceAlgo<>(board, moveAlgo, captureAlgo);
     }
 
     @Override
     public Collection<Position> calculate(PAWN pawn) {
-        return algos.stream()
-                .map(algo -> algo.calculate(pawn))
-                .flatMap(Collection::stream)
+        var positions = algo.calculate(pawn);
+        return positions.stream()
                 .filter(position -> position.y() == promotionLine)
                 .toList();
     }
