7d68c779f8d0bc6678aedbf55adc362a72f1dae2
Andrii Gutsul <agutsulrepo@gmail.com>
2025-01-06 08:03:01 -0500

    fix wrong board state extension

--- src/main/java/com/agutsul/chess/board/state/AgreedWinBoardState.java
+++ src/main/java/com/agutsul/chess/board/state/AgreedWinBoardState.java
@@ -1,18 +1,36 @@
 package com.agutsul.chess.board.state;
 
+import static java.util.Collections.emptyList;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.Collection;
 
 import org.slf4j.Logger;
 
+import com.agutsul.chess.activity.action.Action;
+import com.agutsul.chess.activity.impact.Impact;
 import com.agutsul.chess.board.Board;
 import com.agutsul.chess.color.Color;
+import com.agutsul.chess.piece.Piece;
 
 public final class AgreedWinBoardState
-        extends AbstractDrawBoardState {
+        extends AbstractBoardState {
 
     private static final Logger LOGGER = getLogger(AgreedWinBoardState.class);
 
     public AgreedWinBoardState(Board board, Color color) {
-        super(LOGGER, BoardState.Type.AGREED_WIN, board, color);
+        super(BoardState.Type.AGREED_WIN, board, color);
+    }
+
+    @Override
+    public final Collection<Action<?>> getActions(Piece<?> piece) {
+        LOGGER.info("Getting actions for piece '{}'", piece);
+        return emptyList();
+    }
+
+    @Override
+    public final Collection<Impact<?>> getImpacts(Piece<?> piece) {
+        LOGGER.info("Getting impacts for piece '{}'", piece);
+        return emptyList();
     }
 }
