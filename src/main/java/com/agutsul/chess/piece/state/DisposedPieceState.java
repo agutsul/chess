a766f07730341f7634c901e7eb5004b315551d97
Andrii Gutsul <agutsulrepo@gmail.com>
2025-02-13 19:01:26 -0500

    fix missed code

--- src/main/java/com/agutsul/chess/piece/state/DisposedPieceState.java
+++ src/main/java/com/agutsul/chess/piece/state/DisposedPieceState.java
@@ -4,7 +4,10 @@ import java.time.Instant;
 
 import com.agutsul.chess.Disposable;
 import com.agutsul.chess.piece.Piece;
+import com.agutsul.chess.state.State;
 
-public interface DisposedPieceState<PIECE extends Piece<?> & Disposable> {
+public interface DisposedPieceState<PIECE extends Piece<?> & Disposable>
+        extends State<PIECE> {
+
     Instant getDisposedAt();
 }
