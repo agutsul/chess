6735d126d4b746747da07adfc4b6ef0152fe14f8
Andrii Gutsul <agutsulrepo@gmail.com>
2025-02-26 08:32:20 -0500

    remove "rawtype" related to generics

--- src/main/java/com/agutsul/chess/activity/action/memento/CancelActionMementoFactory.java
+++ src/main/java/com/agutsul/chess/activity/action/memento/CancelActionMementoFactory.java
@@ -7,6 +7,10 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
+import com.agutsul.chess.Capturable;
+import com.agutsul.chess.Castlingable;
+import com.agutsul.chess.Demotable;
+import com.agutsul.chess.Movable;
 import com.agutsul.chess.activity.action.Action;
 import com.agutsul.chess.activity.action.CancelCaptureAction;
 import com.agutsul.chess.activity.action.CancelCastlingAction;
@@ -15,8 +19,11 @@ import com.agutsul.chess.activity.action.CancelEnPassantAction;
 import com.agutsul.chess.activity.action.CancelMoveAction;
 import com.agutsul.chess.activity.action.CancelPromoteAction;
 import com.agutsul.chess.board.Board;
+import com.agutsul.chess.color.Color;
 import com.agutsul.chess.exception.IllegalActionException;
 import com.agutsul.chess.piece.PawnPiece;
+import com.agutsul.chess.piece.Piece;
+import com.agutsul.chess.position.Position;
 
 public enum CancelActionMementoFactory
         implements BiFunction<Board,ActionMemento<?,?>,Action<?>> {
@@ -24,20 +31,27 @@ public enum CancelActionMementoFactory
     MOVE_MODE(Action.Type.MOVE) {
 
         @Override
-        @SuppressWarnings({ "unchecked", "rawtypes" })
+        @SuppressWarnings("unchecked")
         public Action<?> apply(Board board, ActionMemento<?,?> memento) {
             var actionMemento = (ActionMemento<String,String>) memento;
 
             var piece = board.getPiece(actionMemento.getTarget());
             var position = board.getPosition(actionMemento.getSource());
 
-            return new CancelMoveAction(piece.get(), position.get());
+            return create(piece.get(), position.get());
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR extends Color,PIECE extends Piece<COLOR> & Movable>
+                CancelMoveAction<COLOR,PIECE> create(Piece<Color> piece, Position position) {
+
+            return new CancelMoveAction<>((PIECE) piece, position);
         }
     },
     CAPTURE_MODE(Action.Type.CAPTURE) {
 
         @Override
-        @SuppressWarnings({ "unchecked", "rawtypes" })
+        @SuppressWarnings("unchecked")
         public Action<?> apply(Board board, ActionMemento<?,?> memento) {
             var actionMemento = (ActionMemento<String,String>) memento;
 
@@ -47,7 +61,15 @@ public enum CancelActionMementoFactory
                     actionMemento.getColor().invert()
             );
 
-            return new CancelCaptureAction(predator.get(), victim.get());
+            return create(predator.get(), victim.get());
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR1 extends Color,COLOR2 extends Color,
+                        PIECE1 extends Piece<COLOR1> & Capturable,PIECE2 extends Piece<COLOR2>>
+                CancelCaptureAction<COLOR1,COLOR2,PIECE1,PIECE2> create(Piece<Color> predator, Piece<Color> victim) {
+
+            return new CancelCaptureAction<>((PIECE1) predator, (PIECE2) victim);
         }
     },
     PROMOTE_MODE(Action.Type.PROMOTE) {
@@ -56,7 +78,6 @@ public enum CancelActionMementoFactory
                 "Unsupported promotion action";
 
         @Override
-        @SuppressWarnings({ "unchecked", "rawtypes" })
         public Action<?> apply(Board board, ActionMemento<?,?> memento) {
             var actionMemento = (PromoteActionMemento) memento;
             var originMemento = actionMemento.getTarget();
@@ -65,9 +86,9 @@ public enum CancelActionMementoFactory
 
             switch (originMemento.getActionType()) {
             case Action.Type.MOVE:
-                return new CancelPromoteAction((CancelMoveAction<?,?>) action);
+                return create((CancelMoveAction<?,?>) action);
             case Action.Type.CAPTURE:
-                return new CancelPromoteAction((CancelCaptureAction<?,?,?,?>) action);
+                return create((CancelCaptureAction<?,?,?,?>) action);
             default:
                 throw new IllegalActionException(String.format("%s: %s",
                         UNSUPPORTED_ACTION_MESSAGE,
@@ -75,31 +96,65 @@ public enum CancelActionMementoFactory
                 ));
             }
         }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR extends Color,
+                        PIECE extends Piece<COLOR> & Movable & Capturable & Demotable>
+                CancelPromoteAction<COLOR,PIECE> create(CancelMoveAction<?,?> action) {
+
+            return new CancelPromoteAction<>((CancelMoveAction<COLOR,PIECE>) action);
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR1 extends Color,
+                        COLOR2 extends Color,
+                        PIECE1 extends Piece<COLOR1> & Movable & Capturable & Demotable,
+                        PIECE2 extends Piece<COLOR2>>
+                CancelPromoteAction<COLOR1,PIECE1> create(CancelCaptureAction<?,?,?,?> action) {
+
+            return new CancelPromoteAction<>((CancelCaptureAction<COLOR1,COLOR2,PIECE1,PIECE2>) action);
+        }
     },
     CASTLING_MODE(Action.Type.CASTLING) {
 
         @Override
-        @SuppressWarnings({ "unchecked", "rawtypes" })
         public Action<?> apply(Board board, ActionMemento<?,?> memento) {
             var castlingMemento = (CastlingActionMemento) memento;
 
             var kingAction = uncastlingAction(board, castlingMemento.getSource());
             var rookAction = uncastlingAction(board, castlingMemento.getTarget());
 
-            return new CancelCastlingAction(
-                    castlingMemento.getSide(),
-                    kingAction,
-                    rookAction
-            );
+            return create(castlingMemento.getSide(), kingAction, rookAction);
         }
 
-        @SuppressWarnings({ "unchecked", "rawtypes" })
         private static UncastlingMoveAction<?,?> uncastlingAction(Board board,
                                                                   ActionMemento<String,String> memento) {
             var piece = board.getPiece(memento.getTarget());
             var position = board.getPosition(memento.getSource());
 
-            return new UncastlingMoveAction(piece.get(), position.get());
+            return create(piece.get(), position.get());
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR extends Color,PIECE extends Piece<COLOR> & Castlingable & Movable>
+                UncastlingMoveAction<COLOR,PIECE> create(Piece<Color> piece, Position position) {
+
+            return new UncastlingMoveAction<>((PIECE) piece, position);
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR extends Color,
+                        PIECE1 extends Piece<COLOR> & Castlingable & Movable,
+                        PIECE2 extends Piece<COLOR> & Castlingable & Movable>
+                CancelCastlingAction<COLOR,PIECE1,PIECE2> create(Castlingable.Side side,
+                                                                 UncastlingMoveAction<?,?> sourceAction,
+                                                                 UncastlingMoveAction<?,?> targetAction) {
+
+            return new CancelCastlingAction<>(
+                    side,
+                    (UncastlingMoveAction<COLOR,PIECE1>) sourceAction,
+                    (UncastlingMoveAction<COLOR,PIECE2>) targetAction
+            );
         }
     },
     EN_PASSANT_MODE(Action.Type.EN_PASSANT) {
@@ -115,10 +170,15 @@ public enum CancelActionMementoFactory
                     actionMemento.getColor().invert()
             );
 
-            return new CancelEnPassantAction<>(
-                    (PawnPiece<?>) predator.get(),
-                    (PawnPiece<?>) victim.get()
-            );
+            return create(predator.get(), victim.get());
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <COLOR1 extends Color,COLOR2 extends Color,
+                        PAWN1 extends PawnPiece<COLOR1>,PAWN2 extends PawnPiece<COLOR2>>
+                CancelEnPassantAction<COLOR1,COLOR2,PAWN1,PAWN2> create(Piece<Color> predator, Piece<Color> victim) {
+
+            return new CancelEnPassantAction<>((PAWN1) predator, (PAWN2) victim);
         }
     };
 
