633b58f4692790d019f69b7a38f5b93d5b7a8ccf
Andrii Gutsul <agutsulrepo@gmail.com>
2025-03-25 18:02:04 -0400

    remove unused method

--- src/main/java/com/agutsul/chess/board/BoardContext.java
+++ src/main/java/com/agutsul/chess/board/BoardContext.java
@@ -22,18 +22,12 @@ final class BoardContext<T>
     public List<T> getKingPositions() {
         return kingPositions;
     }
-    public void setKingPositions(List<T> kingPositions) {
-        this.kingPositions = kingPositions;
-    }
 
     public void addQueenPosition(T position) {
         this.queenPositions.add(position);
     }
     public List<T> getQueenPositions() {
         return queenPositions;
-    }
-    public void setQueenPositions(List<T> queenPositions) {
-        this.queenPositions = queenPositions;
     }
 
     public void addBishopPosition(T position) {
@@ -42,18 +36,12 @@ final class BoardContext<T>
     public List<T> getBishopPositions() {
         return bishopPositions;
     }
-    public void setBishopPositions(List<T> bishopPositions) {
-        this.bishopPositions = bishopPositions;
-    }
 
     public void addKnightPosition(T position) {
         this.knightPositions.add(position);
     }
     public List<T> getKnightPositions() {
         return knightPositions;
-    }
-    public void setKnightPositions(List<T> knightPositions) {
-        this.knightPositions = knightPositions;
     }
 
     public void addRookPosition(T position) {
@@ -62,17 +50,11 @@ final class BoardContext<T>
     public List<T> getRookPositions() {
         return rookPositions;
     }
-    public void setRookPositions(List<T> rookPositions) {
-        this.rookPositions = rookPositions;
-    }
 
     public void addPawnPosition(T position) {
         this.pawnPositions.add(position);
     }
     public List<T> getPawnPositions() {
         return pawnPositions;
-    }
-    public void setPawnPositions(List<T> pawnPositions) {
-        this.pawnPositions = pawnPositions;
     }
 }
