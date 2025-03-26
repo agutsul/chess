8f0bcf34e19b6793081f9a91321d4491d1eddf17
Andrii Gutsul <agutsulrepo@gmail.com>
2025-03-25 22:14:40 -0400

    add test

---
+++ src/test/java/com/agutsul/chess/ai/SimulatedPiecePromoteActionTest.java
@@ -0,0 +1,45 @@
+package com.agutsul.chess.ai;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.agutsul.chess.activity.action.PieceCaptureAction;
+import com.agutsul.chess.activity.action.PieceMoveAction;
+import com.agutsul.chess.event.Observable;
+import com.agutsul.chess.piece.Piece;
+
+@ExtendWith(MockitoExtension.class)
+public class SimulatedPiecePromoteActionTest {
+
+    @Test
+    void testPromoteMoveAction() {
+        var pieceType = Piece.Type.QUEEN;
+
+        @SuppressWarnings("unchecked")
+        var promoteAction = new SimulatedPiecePromoteAction<>(
+                mock(PieceMoveAction.class),
+                mock(Observable.class),
+                pieceType
+        );
+
+        assertEquals(pieceType, promoteAction.getPieceType());
+    }
+
+    @Test
+    void testPromoteCaptureAction() {
+        var pieceType = Piece.Type.QUEEN;
+
+        @SuppressWarnings("unchecked")
+        var promoteAction = new SimulatedPiecePromoteAction<>(
+                mock(PieceCaptureAction.class),
+                mock(Observable.class),
+                pieceType
+        );
+
+        assertEquals(pieceType, promoteAction.getPieceType());
+    }
+}
