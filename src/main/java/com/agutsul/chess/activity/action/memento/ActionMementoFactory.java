881da40487a5583ea961b2bfe46aa20c5e169087
Andrii Gutsul <agutsulrepo@gmail.com>
2025-03-26 20:43:16 -0400

    re-factor ActionMementoFactory

--- src/main/java/com/agutsul/chess/activity/action/memento/ActionMementoFactory.java
+++ src/main/java/com/agutsul/chess/activity/action/memento/ActionMementoFactory.java
@@ -21,129 +21,11 @@ import com.agutsul.chess.board.Board;
 import com.agutsul.chess.piece.Piece;
 import com.agutsul.chess.position.Position;
 
-public enum ActionMementoFactory
-        implements Function<Action<?>,ActionMemento<?,?>> {
-
-    MOVE_MODE(Action.Type.MOVE),
-    BIG_MOVE_MODE(Action.Type.BIG_MOVE),
-    CAPTURE_MODE(Action.Type.CAPTURE),
-    PROMOTE_MODE(Action.Type.PROMOTE) {
-
-        @Override
-        public ActionMemento<?,?> apply(Action<?> action) {
-            var promoteAction = (PiecePromoteAction<?,?>) action;
-            var originAction = (Action<?>) promoteAction.getSource();
-
-            @SuppressWarnings("unchecked")
-            var memento = (ActionMemento<String,String>) super.apply(originAction);
-            return new PromoteActionMemento(
-                    promoteAction.getType(),
-                    new PieceTypeLazyInitializer(promoteAction),
-                    memento
-            );
-        }
-
-        static final class PieceTypeLazyInitializer
-                extends LazyInitializer<Piece.Type> {
-
-            private final PiecePromoteAction<?,?> action;
-
-            PieceTypeLazyInitializer(PiecePromoteAction<?,?> action) {
-                this.action = action;
-            }
-
-            @Override
-            protected Piece.Type initialize() {
-                return action.getPieceType();
-            }
-        }
-    },
-    CASTLING_MODE(Action.Type.CASTLING) {
-
-        @Override
-        public ActionMemento<?,?> apply(Action<?> action) {
-            var castlingAction = (PieceCastlingAction<?,?,?>) action;
-
-            return new CastlingActionMemento(
-                    castlingAction.getSide(),
-                    castlingAction.getType(),
-                    createMemento(castlingAction.getSource()),
-                    createMemento(castlingAction.getTarget())
-            );
-        }
-
-        @SuppressWarnings("unchecked")
-        private ActionMemento<String,String> createMemento(CastlingMoveAction<?,?> action) {
-            return (ActionMemento<String,String>) super.apply(action);
-        }
-    },
-    EN_PASSANT_MODE(Action.Type.EN_PASSANT) {
-
-        @Override
-        public ActionMemento<?,?> apply(Action<?> action) {
-            var enPassantAction = (PieceEnPassantAction<?,?,?,?>) action;
-
-            var sourcePawn = enPassantAction.getSource();
-            var targetPawn = enPassantAction.getTarget();
-
-            var memento = createMemento(
-                    Action.Type.CAPTURE,
-                    sourcePawn,
-                    targetPawn.getPosition()
-            );
-
-            return new EnPassantActionMemento(
-                    enPassantAction.getType(),
-                    memento,
-                    enPassantAction.getPosition()
-            );
-        }
-    };
-
-    private static final Map<Action.Type, ActionMementoFactory> MODES =
-                Stream.of(values()).collect(toMap(ActionMementoFactory::type, identity()));
-
-    private Action.Type type;
-
-    ActionMementoFactory(Action.Type type) {
-        this.type = type;
-    }
-
-    private Action.Type type() {
-        return type;
-    }
-
-    @Override
-    public ActionMemento<?,?> apply(Action<?> action) {
-        return createMemento(action.getType(), action.getPiece(), action.getPosition());
-    }
-
-    static ActionMemento<String,String> createMemento(Action.Type actionType,
-                                                      Piece<?> sourcePiece,
-                                                      Position targetPosition) {
-
-        return new ActionMementoImpl<>(
-                sourcePiece.getColor(),
-                actionType,
-                sourcePiece.getType(),
-                String.valueOf(sourcePiece.getPosition()),
-                String.valueOf(targetPosition)
-        );
-    }
-
-    private static String createCode(Piece<?> piece, Position position) {
-        var sourcePosition = piece.getPosition();
-        var code = String.valueOf(sourcePosition);
-
-        var label = sourcePosition.x() == position.x()
-                ? code.charAt(1)  // y
-                : code.charAt(0); // x
-
-        return String.valueOf(label);
-    }
+public enum ActionMementoFactory {
+    INSTANCE;
 
     public static ActionMemento<?,?> createMemento(Board board, Action<?> action) {
-        var memento = MODES.get(action.getType()).apply(action);
+        var memento = FactoryMode.MODES.get(action.getType()).apply(action);
         if (isCastling(action)) {
             return memento;
         }
@@ -180,5 +62,125 @@ public enum ActionMementoFactory
         return optionalMemento.isPresent()
                 ? optionalMemento.get()
                 : memento;
+    }
+
+    private static String createCode(Piece<?> piece, Position position) {
+        var sourcePosition = piece.getPosition();
+        var label = String.valueOf(sourcePosition);
+
+        var code = sourcePosition.x() == position.x()
+                ? label.charAt(1)  // y
+                : label.charAt(0); // x
+
+        return String.valueOf(code);
+    }
+
+    private enum FactoryMode implements Function<Action<?>,ActionMemento<?,?>> {
+        MOVE_MODE(Action.Type.MOVE),
+        BIG_MOVE_MODE(Action.Type.BIG_MOVE),
+        CAPTURE_MODE(Action.Type.CAPTURE),
+        PROMOTE_MODE(Action.Type.PROMOTE) {
+
+            @Override
+            public ActionMemento<?,?> apply(Action<?> action) {
+                var promoteAction = (PiecePromoteAction<?,?>) action;
+                var originAction  = (Action<?>) promoteAction.getSource();
+
+                @SuppressWarnings("unchecked")
+                var memento = (ActionMemento<String,String>) super.apply(originAction);
+                return new PromoteActionMemento(
+                        promoteAction.getType(),
+                        new PieceTypeLazyInitializer(promoteAction),
+                        memento
+                );
+            }
+
+            static final class PieceTypeLazyInitializer
+                    extends LazyInitializer<Piece.Type> {
+
+                private final PiecePromoteAction<?,?> action;
+
+                PieceTypeLazyInitializer(PiecePromoteAction<?,?> action) {
+                    this.action = action;
+                }
+
+                @Override
+                protected Piece.Type initialize() {
+                    return action.getPieceType();
+                }
+            }
+        },
+        CASTLING_MODE(Action.Type.CASTLING) {
+
+            @Override
+            public ActionMemento<?,?> apply(Action<?> action) {
+                var castlingAction = (PieceCastlingAction<?,?,?>) action;
+
+                return new CastlingActionMemento(
+                        castlingAction.getSide(),
+                        castlingAction.getType(),
+                        createMemento(castlingAction.getSource()),
+                        createMemento(castlingAction.getTarget())
+                );
+            }
+
+            @SuppressWarnings("unchecked")
+            private ActionMemento<String,String> createMemento(CastlingMoveAction<?,?> action) {
+                return (ActionMemento<String,String>) super.apply(action);
+            }
+        },
+        EN_PASSANT_MODE(Action.Type.EN_PASSANT) {
+
+            @Override
+            public ActionMemento<?,?> apply(Action<?> action) {
+                var enPassantAction = (PieceEnPassantAction<?,?,?,?>) action;
+
+                var sourcePawn = enPassantAction.getSource();
+                var targetPawn = enPassantAction.getTarget();
+
+                var memento = createMemento(
+                        Action.Type.CAPTURE,
+                        sourcePawn,
+                        targetPawn.getPosition()
+                );
+
+                return new EnPassantActionMemento(
+                        enPassantAction.getType(),
+                        memento,
+                        enPassantAction.getPosition()
+                );
+            }
+        };
+
+        private static final Map<Action.Type,FactoryMode> MODES =
+                    Stream.of(values()).collect(toMap(FactoryMode::type, identity()));
+
+        private Action.Type type;
+
+        FactoryMode(Action.Type type) {
+            this.type = type;
+        }
+
+        private Action.Type type() {
+            return type;
+        }
+
+        @Override
+        public ActionMemento<?,?> apply(Action<?> action) {
+            return createMemento(action.getType(), action.getPiece(), action.getPosition());
+        }
+
+        static ActionMemento<String,String> createMemento(Action.Type actionType,
+                                                          Piece<?> sourcePiece,
+                                                          Position targetPosition) {
+
+            return new ActionMementoImpl<>(
+                    sourcePiece.getColor(),
+                    actionType,
+                    sourcePiece.getType(),
+                    String.valueOf(sourcePiece.getPosition()),
+                    String.valueOf(targetPosition)
+            );
+        }
     }
 }
