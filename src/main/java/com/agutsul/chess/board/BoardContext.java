8feeaf253a2db4fb178bec354e89e97f7c167113
Andrii Gutsul <agutsulrepo@gmail.com>
2026-01-10 13:39:11 -0500

    minor BoardContext improvement

--- src/main/java/com/agutsul/chess/board/BoardContext.java
+++ src/main/java/com/agutsul/chess/board/BoardContext.java
@@ -1,60 +1,69 @@
 package com.agutsul.chess.board;
 
+import static java.util.List.copyOf;
+
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-final class BoardContext<POSITION>
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
+
+import com.agutsul.chess.piece.Piece;
+
+final class BoardContext<POSITION extends Serializable>
         implements Serializable {
 
     private static final long serialVersionUID = -111134609281991106L;
 
-    private List<POSITION> kingPositions = new ArrayList<>();
-    private List<POSITION> queenPositions = new ArrayList<>();
-    private List<POSITION> bishopPositions = new ArrayList<>();
-    private List<POSITION> knightPositions = new ArrayList<>();
-    private List<POSITION> rookPositions = new ArrayList<>();
-    private List<POSITION> pawnPositions = new ArrayList<>();
+    private MultiValuedMap<Piece.Type,POSITION> positions;
+
+    BoardContext() {
+        this.positions = new ArrayListValuedHashMap<>();
+    }
 
     public void addKingPosition(POSITION position) {
-        this.kingPositions.add(position);
+        this.positions.put(Piece.Type.KING, position);
     }
     public List<POSITION> getKingPositions() {
-        return kingPositions;
+        return getPositions(Piece.Type.KING);
     }
 
     public void addQueenPosition(POSITION position) {
-        this.queenPositions.add(position);
+        this.positions.put(Piece.Type.QUEEN, position);
     }
     public List<POSITION> getQueenPositions() {
-        return queenPositions;
+        return getPositions(Piece.Type.QUEEN);
     }
 
     public void addBishopPosition(POSITION position) {
-        this.bishopPositions.add(position);
+        this.positions.put(Piece.Type.BISHOP, position);
     }
     public List<POSITION> getBishopPositions() {
-        return bishopPositions;
+        return getPositions(Piece.Type.BISHOP);
     }
 
     public void addKnightPosition(POSITION position) {
-        this.knightPositions.add(position);
+        this.positions.put(Piece.Type.KNIGHT, position);
     }
     public List<POSITION> getKnightPositions() {
-        return knightPositions;
+        return getPositions(Piece.Type.KNIGHT);
     }
 
     public void addRookPosition(POSITION position) {
-        this.rookPositions.add(position);
+        this.positions.put(Piece.Type.ROOK, position);
     }
     public List<POSITION> getRookPositions() {
-        return rookPositions;
+        return getPositions(Piece.Type.ROOK);
     }
 
     public void addPawnPosition(POSITION position) {
-        this.pawnPositions.add(position);
+        this.positions.put(Piece.Type.PAWN, position);
     }
     public List<POSITION> getPawnPositions() {
-        return pawnPositions;
+        return getPositions(Piece.Type.PAWN);
+    }
+
+    private List<POSITION> getPositions(Piece.Type pieceType) {
+        return copyOf(this.positions.get(pieceType));
     }
 }
