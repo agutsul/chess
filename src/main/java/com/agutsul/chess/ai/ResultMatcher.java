8d34c53d48fadc0df29964cd20c23194aab883ab
Andrii Gutsul <agutsulrepo@gmail.com>
2026-01-17 03:36:12 -0500

    code style improvement

--- src/main/java/com/agutsul/chess/ai/ResultMatcher.java
+++ src/main/java/com/agutsul/chess/ai/ResultMatcher.java
@@ -3,7 +3,7 @@ package com.agutsul.chess.ai;
 import com.agutsul.chess.activity.action.Action;
 
 interface ResultMatcher<ACTION extends Action<?>,
-                        VALUE extends Comparable<VALUE>,
+                        VALUE  extends Comparable<VALUE>,
                         RESULT extends TaskResult<ACTION,VALUE>> {
 
     boolean match(RESULT result);
--- src/main/java/com/agutsul/chess/ai/SimulationTask.java
+++ src/main/java/com/agutsul/chess/ai/SimulationTask.java
@@ -3,7 +3,7 @@ package com.agutsul.chess.ai;
 import com.agutsul.chess.activity.action.Action;
 
 interface SimulationTask<ACTION extends Action<?>,
-                         VALUE extends Comparable<VALUE>,
+                         VALUE  extends Comparable<VALUE>,
                          RESULT extends TaskResult<ACTION,VALUE>> {
 
     RESULT simulate(ACTION action);
