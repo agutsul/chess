66b9176cd5fa5a79f77b9c19df2a6aeb0a0f280c
Andrii Gutsul <agutsulrepo@gmail.com>
2026-01-22 11:56:54 -0500

    re-factoring king rules - remove unused classes

--- src/main/java/com/agutsul/chess/piece/king/KingPieceActionRule.java
+++ src/main/java/com/agutsul/chess/piece/king/KingPieceActionRule.java
@@ -4,8 +4,11 @@ import com.agutsul.chess.activity.action.Action;
 import com.agutsul.chess.board.Board;
 import com.agutsul.chess.color.Color;
 import com.agutsul.chess.piece.KingPiece;
+import com.agutsul.chess.piece.king.KingPieceAlgoProxy.Mode;
 import com.agutsul.chess.rule.AbstractPieceRule;
 import com.agutsul.chess.rule.CompositePieceRule;
+import com.agutsul.chess.rule.action.PieceCapturePositionActionRule;
+import com.agutsul.chess.rule.action.PieceMovePositionActionRule;
 
 public final class KingPieceActionRule<COLOR extends Color,
                                        PIECE extends KingPiece<COLOR>>
@@ -19,8 +22,8 @@ public final class KingPieceActionRule<COLOR extends Color,
     private KingPieceActionRule(Board board, KingPieceAlgo<COLOR,PIECE> algo) {
         super(new CompositePieceRule<>(
                 new KingCastlingActionRule<>(board),
-                new KingCaptureActionRule<>(board, algo),
-                new KingMoveActionRule<>(board, algo)
+                new PieceCapturePositionActionRule<>(board, new KingPieceAlgoProxy<>(Mode.CAPTURE, board, algo)),
+                new PieceMovePositionActionRule<>(board, new KingPieceAlgoProxy<>(Mode.MOVE, board, algo))
             )
         );
     }
--- src/main/java/com/agutsul/chess/piece/king/KingPieceImpactRule.java
+++ src/main/java/com/agutsul/chess/piece/king/KingPieceImpactRule.java
@@ -4,10 +4,17 @@ import com.agutsul.chess.activity.impact.Impact;
 import com.agutsul.chess.board.Board;
 import com.agutsul.chess.color.Color;
 import com.agutsul.chess.piece.KingPiece;
+import com.agutsul.chess.piece.king.KingPieceAlgoProxy.Mode;
 import com.agutsul.chess.rule.AbstractPieceRule;
 import com.agutsul.chess.rule.CompositePieceRule;
+import com.agutsul.chess.rule.impact.attack.PieceDiscoveredAttackPositionImpactRule;
 import com.agutsul.chess.rule.impact.control.PieceControlPositionImpactRule;
+import com.agutsul.chess.rule.impact.domination.PieceDominationPositionImpactRule;
+import com.agutsul.chess.rule.impact.fork.PieceForkPositionImpactRule;
+import com.agutsul.chess.rule.impact.outpost.PieceOutpostPositionImpactRule;
+import com.agutsul.chess.rule.impact.overloading.PieceOverloadingPositionImpactRule;
 import com.agutsul.chess.rule.impact.protect.PieceProtectPositionImpactRule;
+import com.agutsul.chess.rule.impact.undermining.PieceUnderminingPositionImpactRule;
 
 public final class KingPieceImpactRule<COLOR extends Color,
                                        PIECE extends KingPiece<COLOR>>
@@ -22,12 +29,12 @@ public final class KingPieceImpactRule<COLOR extends Color,
         super(new CompositePieceRule<>(
                 new PieceProtectPositionImpactRule<>(board, algo),
                 new PieceControlPositionImpactRule<>(board, algo),
-                new KingForkImpactRule<>(board, algo),
-                new KingDiscoveredAttackImpactRule<>(board, algo),
-                new KingOverloadingImpactRule<>(board, algo),
-                new KingUnderminingImpactRule<>(board, algo),
-                new KingOutpostImpactRule<>(board, algo),
-                new KingDominationImpactRule<>(board, algo)
+                new PieceForkPositionImpactRule<>(board, new KingPieceAlgoProxy<>(Mode.CAPTURE, board, algo)),
+                new PieceDiscoveredAttackPositionImpactRule<>(board, new KingPieceAlgoProxy<>(Mode.DEFAULT, board, algo)),
+                new PieceOverloadingPositionImpactRule<>(board, new KingPieceAlgoProxy<>(Mode.CAPTURE, board, algo)),
+                new PieceUnderminingPositionImpactRule<>(board, new KingPieceAlgoProxy<>(Mode.CAPTURE, board, algo)),
+                new PieceOutpostPositionImpactRule<>(board,  new KingPieceAlgoProxy<>(Mode.MOVE, board, algo)),
+                new PieceDominationPositionImpactRule<>(board, new KingPieceAlgoProxy<>(Mode.CAPTURE, board, algo))
             )
         );
     }
