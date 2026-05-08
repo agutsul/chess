73c0efb7b55d72f7000f0b6eab975d17e8a732fe
Andrii Gutsul <agutsulrepo@gmail.com>
2026-05-08 17:58:30 -0500

    re-factoring: remove unused interface

--- src/main/java/com/agutsul/chess/Placeable.java
+++
@@ -1,5 +0,0 @@
-package com.agutsul.chess;
-
-public interface Placeable {
-
-}
--- src/main/java/com/agutsul/chess/position/Position.java
+++ src/main/java/com/agutsul/chess/position/Position.java
@@ -3,11 +3,10 @@ package com.agutsul.chess.position;
 import java.io.Serializable;
 
 import com.agutsul.chess.Calculatable;
-import com.agutsul.chess.Placeable;
 import com.agutsul.chess.color.Color;
 
 public interface Position
-        extends Calculatable, Placeable, Serializable {
+        extends Calculatable, Serializable {
 
     String[] LABELS = { "a", "b", "c", "d", "e", "f", "g", "h" };
 
