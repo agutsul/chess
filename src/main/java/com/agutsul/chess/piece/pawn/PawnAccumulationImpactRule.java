f771a17dcce761ccb71fcbaed271f22c459184f8
Andrii Gutsul <agutsulrepo@gmail.com>
2026-07-01 11:52:53 -0500

    pawn accumulation impact: sort impacted pawns

--- src/main/java/com/agutsul/chess/piece/pawn/PawnAccumulationImpactRule.java
+++ src/main/java/com/agutsul/chess/piece/pawn/PawnAccumulationImpactRule.java
@@ -1,8 +1,10 @@
 package com.agutsul.chess.piece.pawn;
 
 import static java.util.Collections.emptyList;
+import static java.util.Comparator.comparing;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.collections4.MultiValuedMap;
@@ -14,6 +16,8 @@ import com.agutsul.chess.board.Board;
 import com.agutsul.chess.color.Color;
 import com.agutsul.chess.piece.PawnPiece;
 import com.agutsul.chess.piece.Piece;
+import com.agutsul.chess.position.Position;
+import com.agutsul.chess.position.PositionComparator;
 import com.agutsul.chess.rule.AbstractRule;
 import com.agutsul.chess.rule.impact.AccumulationImpactRule;
 
@@ -22,6 +26,8 @@ final class PawnAccumulationImpactRule<COLOR extends Color,
                                        PAWN  extends PawnPiece<COLOR>>
         extends AbstractRule<PAWN,PieceAccumulationImpact<COLOR,PAWN>,Impact.Type>
         implements AccumulationImpactRule<COLOR,PAWN,PieceAccumulationImpact<COLOR,PAWN>> {
+
+    private static final Comparator<Position> POSITION_COMPARATOR = new PositionComparator();
 
     PawnAccumulationImpactRule(Board board) {
         super(board, Impact.Type.ACCUMULATION);
@@ -37,8 +43,14 @@ final class PawnAccumulationImpactRule<COLOR extends Color,
 
         var pawns = pawnPositions.get(createKey(piece));
         return pawns.size() > 1
-                ? List.of(new PieceAccumulationImpact<>(pawns))
+                ? List.of(createImpact(pawns))
                 : emptyList();
+    }
+
+    private PieceAccumulationImpact<COLOR,PAWN> createImpact(Collection<PAWN> pawns) {
+        return new PieceAccumulationImpact<>(
+                pawns.stream().sorted(comparing(Piece::getPosition, POSITION_COMPARATOR)).toList()
+        );
     }
 
     private static Integer createKey(Piece<?> piece) {
